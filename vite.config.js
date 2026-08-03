0cecd3437123d773c075ce83f89eca5f7837d74f
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2026-08-03 16:01:43 +0200

    fix: Player.html Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/mi-web-technologien-beiboot-ss2026-miahenri/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        player: resolve(__dirname, "player.html"),
+      },
+    },
+  },
 });
